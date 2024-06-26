b015043521ac5637f3269bffed8e5627957c1568
jacksonkasi1 <jacksonkasipeacock@gmail.com>
2024-06-26 10:16:08 +0530

    added another on custom web serivces

---
+++ local/custom_service/version.php
@@ -0,0 +1,8 @@
+<?php
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'local_custom_service';
+$plugin->version = 2024042200;
+$plugin->requires = 2024041600;
+$plugin->maturity = MATURITY_STABLE;
+$plugin->release = '1.0';
